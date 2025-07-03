e44858627c1f855cd089084dd9d12b0372fffd90
Achraf Malki <achrafmalki1404@gmail.com>
2025-07-03 20:48:06 +0100

    updated skills section

--- src/sections/SkillsSection/Skills.jsx
+++ src/sections/SkillsSection/Skills.jsx
@@ -6,7 +6,8 @@ import { useTheme } from "../../common/ThemeContext";
 
 function Skills() {
   const { theme, toggleTheme } = useTheme();
-  const checkMarkIcon = theme == 'dark' ? checkMarkIconDark : checkMarkIconLight;
+  const checkMarkIcon =
+    theme == "dark" ? checkMarkIconDark : checkMarkIconLight;
   return (
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
@@ -14,7 +15,13 @@ function Skills() {
         <SkillList src={checkMarkIcon} skill="Java" />
         <SkillList src={checkMarkIcon} skill="Python" />
         <SkillList src={checkMarkIcon} skill="C, C++" />
-        <SkillList src={checkMarkIcon} skill="SQL, PL/SQL, SQL Server" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="MySQL (SQL)" />
+        <SkillList src={checkMarkIcon} skill="Oracle (PL/SQL)" />
+        <SkillList src={checkMarkIcon} skill="SQL Server (T-SQL)" />
+        <SkillList src={checkMarkIcon} skill="PostgreSQL (SQL)" />
       </div>
       <hr />
       <div className={styles.skillList}>
@@ -28,7 +35,7 @@ function Skills() {
           src={checkMarkIcon}
           skill="React, Node.js, Express, MongoDB"
         />
-        <SkillList src={checkMarkIcon} skill="Laravel" />
+        <SkillList src={checkMarkIcon} skill="Laravel, Django" />
         <SkillList src={checkMarkIcon} skill="Git, Linux" />
       </div>
     </section>
